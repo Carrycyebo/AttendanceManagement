0a6a8440688edca27de860aa1157b7d5800778f9
Carrycyebo <1730452337@qq.com>
2024-12-30 20:01:50 +0800

    feat:将处理结果推送到kakfa

--- api/spark/structured_streaming.py
+++ api/spark/structured_streaming.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     # 1- 创建 SparkSession 对象
     spark = SparkSession.builder \
         .config("spark.sql.shuffle.partitions", 1) \
-        .appName('ss_read_kafka_process') \
+        .appName('ss_kafka_push') \
         .master('local[*]') \
         .getOrCreate()
 
@@ -22,10 +22,10 @@ if __name__ == '__main__':
 
     # 3- 解析数据
     parsed_stream = kafka_stream.selectExpr("cast(value as string) as value") \
-        .withColumn("班级号", F.split(F.col("value"), "\t")[0]) \
+        .withColumn("学号", F.split(F.col("value"), "\t")[0]) \
         .withColumn("姓名", F.split(F.col("value"), "\t")[1]) \
-        .withColumn("课程", F.split(F.col("value"), "\t")[2]) \
-        .withColumn("学号", F.split(F.col("value"), "\t")[3]) \
+        .withColumn("班级号", F.split(F.col("value"), "\t")[2]) \
+        .withColumn("课程", F.split(F.col("value"), "\t")[3]) \
         .withColumn("状态", F.split(F.col("value"), "\t")[4]) \
         .drop("value")
 
@@ -46,27 +46,32 @@ if __name__ == '__main__':
         .count() \
         .withColumnRenamed("count", "课程数量")
 
-    # 5- 输出流
-    attendance_query = attendance_summary.writeStream \
+    # 5- 定义一个通用的函数将数据写入 Kafka
+    def write_to_kafka(batch_df, batch_id, topic_name):
+        batch_df.selectExpr(
+            "cast(null as string) as key",
+            "to_json(struct(*)) as value"  # 将所有列转换为 JSON 格式
+        ).write \
+            .format("kafka") \
+            .option("kafka.bootstrap.servers", "zhao:9092") \
+            .option("topic", topic_name) \
+            .save()
+
+    # 6- 将每组统计结果写入不同的 Kafka topic
+    attendance_summary.writeStream \
+        .foreachBatch(lambda df, id: write_to_kafka(df, id, "attendance_summary")) \
         .outputMode("complete") \
-        .format("console") \
-        .trigger(processingTime="2 seconds") \
-        .option("truncate", "false") \
         .start()
 
-    class_query = class_attendance.writeStream \
+    class_attendance.writeStream \
+        .foreachBatch(lambda df, id: write_to_kafka(df, id, "class_attendance")) \
         .outputMode("complete") \
-        .format("console") \
-        .trigger(processingTime="2 seconds") \
-        .option("truncate", "false") \
         .start()
 
-    course_query = course_count.writeStream \
+    course_count.writeStream \
+        .foreachBatch(lambda df, id: write_to_kafka(df, id, "course_count")) \
         .outputMode("complete") \
-        .format("console") \
-        .trigger(processingTime="2 seconds") \
-        .option("truncate", "false") \
         .start()
 
-    # 6- 等待终止
+    # 7- 等待终止
     spark.streams.awaitAnyTermination()
